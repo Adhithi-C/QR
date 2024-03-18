f1afeefa700d9fd23dce5d82c9d23d19f9190d64
Adhithi <adhithi2244@gmail.com>
2024-03-18 16:29:50 +0530

    QR registration

--- lib/Login.dart
+++ lib/Login.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:registration_page/QR.dart';
 import 'package:registration_page/registration.dart';
+import 'package:http/http.dart' as http;
+// import 'package:registration_page/scanner.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -10,6 +14,31 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  TextEditingController roll = TextEditingController();
+  TextEditingController password = TextEditingController();
+  void login() async {
+    Uri uri = Uri.parse('https://scnner-web.onrender.com/api/login');
+    var response = await http.post(uri,
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode({
+          'rollno': roll.text,
+          'password': password.text
+        }));
+    var data= jsonDecode(response.body);
+    print(data["message"]);
+    if (response.statusCode == 200) {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => QR(),
+          ));
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(data["message"])));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,22 +60,26 @@ class _LoginState extends State<Login> {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextField(
+              controller: roll,
               decoration: InputDecoration(
                   enabledBorder: OutlineInputBorder(
                     borderSide: BorderSide(color: Colors.white), //<-- SEE HERE
                   ),
-                  hintText: 'Enter your Roll no', hintStyle: TextStyle(color: Colors.white)),
+                  hintText: 'Enter your Roll no',
+                  hintStyle: TextStyle(color: Colors.white)),
             ),
           ),
           SizedBox(height: 10),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextField(
+              controller: password,
               decoration: InputDecoration(
                   enabledBorder: OutlineInputBorder(
                     borderSide: BorderSide(color: Colors.white), //<-- SEE HERE
                   ),
-                  hintText: 'Enter your Password', hintStyle: TextStyle(color: Colors.white)),
+                  hintText: 'Enter your Password',
+                  hintStyle: TextStyle(color: Colors.white)),
             ),
           ),
           SizedBox(
@@ -55,11 +88,7 @@ class _LoginState extends State<Login> {
           Container(
             child: ElevatedButton(
               onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => QR(),
-                    ));
+              login();
               },
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.teal[700],
@@ -78,6 +107,9 @@ class _LoginState extends State<Login> {
           ),
           TextButton(
               onPressed: () {
+                setState(() {
+                  login();
+                });
                 Navigator.push(
                     context,
                     MaterialPageRoute(
